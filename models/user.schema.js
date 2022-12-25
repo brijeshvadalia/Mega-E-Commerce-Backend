abfeb50b3d717b6e2b5647cf648c7a41abfc87e8
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-25 10:46:40 +0530

    Added user schema

---
+++ models/user.schema.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles"
+
+const userSchema = mongoose.Schema(
+
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+            maxLength: [50, "Name must be less than 50"]
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minLength: [8, "Password must be at least 8 characters"],
+            select: false
+        },
+        role: {
+            type: String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date,
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default mongoose.model("User",userSchema)
