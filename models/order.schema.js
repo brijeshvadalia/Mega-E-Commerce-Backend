ad7e84f2b375249f9a8526370281626c3990bc81
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-29 16:35:20 +0530

    Order Schema Added

--- models/order.schema.js
+++ models/order.schema.js
@@ -14,6 +14,7 @@ const orderSchema = new mongoose.Schema(
                     price: Number
                 }
             ],
+            
             required: true
         },
         user: {
