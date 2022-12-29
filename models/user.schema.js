84d1ed2c50ca926583d3d45f9626a2c4da6ea53a
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-29 10:35:13 +0530

    Generate Forgot password Token added

--- models/user.schema.js
+++ models/user.schema.js
@@ -64,5 +64,21 @@ userSchema.methods = {
             }
         )
     },
+     generateForgotPasswordToken: function(){
+        const forgotToken = crypto.randomBytes(20).toString('hex');
+
+        //step 1 - save to DB
+        this.forgotPasswordToken = crypto
+        .createHash("sha256")
+        .update(forgotToken)
+        .digest("hex")
+
+        this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000
+        //step 2 - return values to user
+
+        return forgotToken
+
+    }
 }
+
 export default mongoose.model("User",userSchema)
