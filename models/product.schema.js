097d07a6d7ca437efa168fa2ca16032e92a16df8
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-29 16:20:56 +0530

    Added Starting Logic to it

--- models/product.schema.js
+++ models/product.schema.js
@@ -1,7 +1,40 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: [true, "Please provide a product name"],
+        trim: true,
+        maxLength: [120, "Product name should be a max of 120 characters"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Please provide a product price"],
+        maxLength: [5, "Product price should not be more than 5 digits"]
+    },
+    description: {
+        type: String,
+    },
+    photos: [
+        {
+            secure_url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    stock: {
+        type: Number,
+        default: 0
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    collectionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Collection"
+    }
 },
 {
     timestamps: true
