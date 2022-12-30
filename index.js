df57c28f32a5e99bb67351101146906abd735de5
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-30 13:21:12 +0530

    Added mongodb connection

---
+++ index.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+import app from './app.js'
+import config from "./config/index"
+
+(async () => {
+    try {
+        await mongoose.connect(config.MONGODB_URL)
+        console.log("DB CONNECTED");
+
+        app.on('error', (err) => {
+            console.log("ERROR: ", err);
+            throw err;
+        })
+
+        const onListening = () => {
+            console.log(`Listening on ${config.PORT}`);
+        }
+
+        app.listen(config.PORT, onListening)
+
+    } catch (err) {
+        console.log("ERROR ", err);
+        throw err
+    }
+})()
+
