f2904796d82a20ff7cc6cbf06da321baccce3e04
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-05 20:55:14 +0530

    Added Code to it

---
+++ utils/mailHelper.js
@@ -0,0 +1,17 @@
+import transporter from '../config/transporter.config.js'
+import config from '../config/index.js'
+
+
+const mailHelper = async (options) => {
+    const message = {
+        from: config.SMTP_MAIL_EMAIL, // sender address
+        to: options.email, // list of receivers
+        subject: options.subject, // Subject line
+        text: options.text, // plain text body
+      }
+
+      await transporter.sendMail(message)
+}
+
+
+export default mailHelper
