028b6122b2215a1204c8a1276441f51524667e6e
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-06 12:35:37 +0530

    Coding for createCollection

--- Controllers/collection.collector.js
+++ Controllers/collection.collector.js
@@ -9,3 +9,24 @@ import CustomError from '../utils/customError'
  * @parameters name, email, password
  * @returns User Object
  ******************************************************/
+
+export const createCollection = asyncHandler(async (req, res) => {
+    //take name from front end
+    const { name } = req.body
+
+    if (!name) {
+        throw new CustomError("Collection name is required", 400)
+    }
+
+    //add this name to database
+    const collection = await Collection.create({
+        name
+    })
+    //send this response value to frontend
+    res.status(200).json({
+        success: true,
+        message: "Collection created with success",
+        collection
+    })
+
+})
