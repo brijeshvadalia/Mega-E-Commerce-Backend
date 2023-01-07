1eea9904e3c24c9ed3ef175fead4506963a738ff
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-07 17:23:17 +0530

    Planning For Deactivate Coupoun Route

--- Controllers/coupons.controller.js
+++ Controllers/coupons.controller.js
@@ -5,3 +5,11 @@
  * @description Only admin and Moderator can create the coupon
  * @returns Coupon Object with success message "Coupon Created SuccessFully"
  *********************************************************/
+
+/**********************************************************
+ * @DEACTIVATE_COUPON
+ * @route https://localhost:5000/api/coupon/deactive/:couponId
+ * @description Controller used for deactivating the coupon
+ * @description Only admin and Moderator can update the coupon
+ * @returns Coupon Object with success message "Coupon Deactivated SuccessFully"
+ *********************************************************/
