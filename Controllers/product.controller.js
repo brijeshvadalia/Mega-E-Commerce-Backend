044c2f06bf7aed3aeb991fb29605125c20618e13
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-07 17:17:50 +0530

    Planning For Add Product Route

--- Controllers/product.controller.js
+++ Controllers/product.controller.js
@@ -6,3 +6,13 @@ import Mongoose from "mongoose"
 import asyncHandler from '../services/asyncHandler'
 import CustomError from '../utils/customError'
 import config from "../config/index.js"
+
+
+/**********************************************************
+ * @ADD_PRODUCT
+ * @route https://localhost:5000/api/product
+ * @description Controller used for creating a new product
+ * @description Only admin can create the coupon
+ * @descriptio Uses AWS S3 Bucket for image upload
+ * @returns Product Object
+ *********************************************************/
