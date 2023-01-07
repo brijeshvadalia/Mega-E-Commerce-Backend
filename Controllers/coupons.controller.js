1f31f1e1c91e54f40e65e527fdac2f0a9722d311
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-07 17:23:39 +0530

    Planning For Delete Coupoun Route

--- Controllers/coupons.controller.js
+++ Controllers/coupons.controller.js
@@ -13,3 +13,11 @@
  * @description Only admin and Moderator can update the coupon
  * @returns Coupon Object with success message "Coupon Deactivated SuccessFully"
  *********************************************************/
+
+/**********************************************************
+ * @DELETE_COUPON
+ * @route https://localhost:5000/api/coupon/:couponId
+ * @description Controller used for deleting the coupon
+ * @description Only admin and Moderator can delete the coupon
+ * @returns Success Message "Coupon Deleted SuccessFully"
+ *********************************************************/
