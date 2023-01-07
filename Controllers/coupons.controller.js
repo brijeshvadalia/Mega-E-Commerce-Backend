5cdd03059aa809037c1008d210c238b845ef9798
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-07 17:22:49 +0530

    Planning For Create Coupoun Route

---
+++ Controllers/coupons.controller.js
@@ -0,0 +1,7 @@
+/**********************************************************
+ * @CREATE_COUPON
+ * @route https://localhost:5000/api/coupon
+ * @description Controller used for creating a new coupon
+ * @description Only admin and Moderator can create the coupon
+ * @returns Coupon Object with success message "Coupon Created SuccessFully"
+ *********************************************************/
