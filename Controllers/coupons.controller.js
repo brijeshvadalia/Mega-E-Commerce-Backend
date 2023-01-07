eb76a596fb089239e03d9e04afdd2312adf01f02
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-07 17:24:06 +0530

    Planning For Get All Coupoun Route

--- Controllers/coupons.controller.js
+++ Controllers/coupons.controller.js
@@ -6,6 +6,7 @@
  * @returns Coupon Object with success message "Coupon Created SuccessFully"
  *********************************************************/
 
+
 /**********************************************************
  * @DEACTIVATE_COUPON
  * @route https://localhost:5000/api/coupon/deactive/:couponId
@@ -14,10 +15,20 @@
  * @returns Coupon Object with success message "Coupon Deactivated SuccessFully"
  *********************************************************/
 
+
 /**********************************************************
  * @DELETE_COUPON
  * @route https://localhost:5000/api/coupon/:couponId
  * @description Controller used for deleting the coupon
  * @description Only admin and Moderator can delete the coupon
  * @returns Success Message "Coupon Deleted SuccessFully"
+ *********************************************************/
+
+
+/**********************************************************
+ * @GET_ALL_COUPONS
+ * @route https://localhost:5000/api/coupon
+ * @description Controller used for getting all coupons details
+ * @description Only admin and Moderator can get all the coupons
+ * @returns allCoupons Object
  *********************************************************/
