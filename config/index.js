4f93a1a04a47928179fc8b4f4367bb78b76d1dea
B-8128 <brijeshvadaliya1@gmail.com>
2023-01-10 16:14:01 +0530

    Added Razorpay necessary Variables

--- config/index.js
+++ config/index.js
@@ -5,17 +5,24 @@ dotenv.config()
 const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+
     MONGODB_URL: process.env.MONGODB_URL,
+
     PORT: process.env.PORT,
+
     SMTP_MAIL_HOST: process.env.SMTP_MAIL_HOST,
     SMTP_MAIL_PORT: process.env.SMTP_MAIL_PORT,
     SMTP_MAIL_USERNAME: process.env.SMTP_MAIL_USERNAME,
     SMTP_MAIL_PASSWORD: process.env.SMTP_MAIL_PASSWORD,
     SMTP_MAIL_EMAIL: process.env.SMTP_MAIL_EMAIL,
+
     S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
     S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
     S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
-    S3_REGION: process.env.S3_REGION
+    S3_REGION: process.env.S3_REGION,
+
+    RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_SECRET: process.env.RAZORPAY_SECRET
 }
 
 export default config;
